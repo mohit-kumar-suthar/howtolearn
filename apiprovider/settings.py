38fb371849df1999bd4ee4a1e9972c7dc30acc4d
mohitsuthar08 <mohitsuthar08@gmail.com>
2021-03-02 16:35:15 +0530

    1

--- apiprovider/settings.py
+++ apiprovider/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v$u@2!qnk1%2b-7r^!s*^(qz1vwvk^ab#s7i2jjb@ixthq9_%w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','howtolearn.herokuapp.com/']
 
